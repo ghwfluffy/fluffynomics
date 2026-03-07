ee52cb6cefedf646bae2d26556673cfe192381e1
GhwFluffy <ghw@ghwfluffy.com>
2026-03-07 08:50:11 -0600

    Moving vue to subdir. Starting prod web Dockerfile

--- go.sh
+++ go.sh
@@ -2,8 +2,16 @@
 
 set -eux -o pipefail
 
+(
+    cd python
+    ./lint.sh
+)
+(
+    cd web
+    ./build.sh
+)
+
 docker compose down -t0
-./lint.sh
 docker compose build
 docker compose up -d
 docker compose logs -f
